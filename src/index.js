ff9226ef873aa123c23f21da2da858f31d49d443
Adrian Gajos <thegajos@gmail.com>
2023-01-27 02:28:41 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import * as dotenv from 'dotenv';
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import App from './App.js';
 import LockService from './Services/LockService.js';
 
 dotenv.config();
+axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
 
 const app = App.getInstance();
 const lockService = new LockService();
